4b3b24bc31a6d0f1d0157b7fc9eee49ff1a884cc
Roman Shtylman <shtylman@gmail.com>
2014-01-14 20:35:53 -0500

    remove stupid module boilerplate shit and make a normal commonjs module

--- hammer.js
+++ hammer.js
@@ -4,9 +4,6 @@
  * Copyright (c) 2014 Jorik Tangelder <j.tangelder@gmail.com>;
  * Licensed under the MIT license */
 
-(function(window, undefined) {
-  'use strict';
-
 /**
  * Hammer
  * use this to create instances
@@ -1360,19 +1357,4 @@ Hammer.gestures.Transform = {
   }
 };
 
-  // Based off Lo-Dash's excellent UMD wrapper (slightly modified) - https://github.com/bestiejs/lodash/blob/master/lodash.js#L5515-L5543
-  // some AMD build optimizers, like r.js, check for specific condition patterns like the following:
-  if(typeof define == 'function' && typeof define.amd == 'object' && define.amd) {
-    // define as an anonymous module
-    define(function() {
-      return Hammer;
-    });
-    // check for `exports` after `define` in case a build optimizer adds an `exports` object
-  }
-  else if(typeof module === 'object' && typeof module.exports === 'object') {
-    module.exports = Hammer;
-  }
-  else {
-    window.Hammer = Hammer;
-  }
-})(this);
+module.exports = Hammer;
